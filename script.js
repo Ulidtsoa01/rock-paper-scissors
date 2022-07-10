ddbe81285fe17ba5913857ef51ed7124130fd2d8
Ulidtsoa01 <daniel.b.h.huang@gmail.com>
2022-07-10 02:27:05 -0500

    a

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let pScore = 0;
+let cScore = 0;
+let winner = "";
+
 function computerPlay() {
     let rand = Math.floor(Math.random()*3);
     switch (rand) {
@@ -12,50 +16,116 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     let win = false;
     if(playerSelection===computerSelection) {
-        return "tie";
+        winner = "tie";
     }
     if(
         (playerSelection==="rock" && computerSelection==="scissors") ||
         (playerSelection==="paper" && computerSelection==="rock") ||
         (playerSelection==="scissors" && computerSelection==="paper")
     ) {
-        return "player";
+        winner = "player";
     } else if(
         (computerSelection==="rock" && playerSelection==="scissors") ||
         (computerSelection==="paper" && playerSelection==="rock") ||
         (computerSelection==="scissors" && playerSelection==="paper")
     ) {
-        return "computer";
-    } else {
-        return "invalid";
-    }
-}
-function game() {
-    let i = 0;
-    let pScore = 0;
-    let cScore = 0;
-    while (i<5) {
-        let p = prompt("Enter choice: ").toLowerCase();
-        let c = computerPlay();
-        let result = playRound(p, c);
-        if(result==="player") {
-            pScore++;
-        }
-        if(result==="computer") {
-            cScore++;
-        }
-        console.log("Round result: "+result);
-        console.log("player chose "+p+", computer chose "+c)
-        i++;
-    }
-    let winner = "";
-    if(pScore>cScore) {
-        winner = "player";
-    } else if(cScore>pScore) {
         winner = "computer";
-    } else if(cScore===pScore) {
-        winner = "tie";
     }
-    console.log("Winner: "+winner);
 }
-game();
+const scoreInfo = document.getElementById('scoreInfo');
+const scoreMessage = document.getElementById('scoreMessage');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
+const playerSign = document.getElementById('playerSign');
+const computerSign = document.getElementById('computerSign');
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const overlay = document.getElementById('overlay');
+const restartBtn = document.getElementById('restartBtn');
+
+rockBtn.addEventListener('click', () => handleClick('rock'));
+paperBtn.addEventListener('click', () => handleClick('paper'));
+scissorsBtn.addEventListener('click', () => handleClick('scissors'));
+restartBtn.addEventListener('click', restartGame);
+overlay.addEventListener('click', closeEndgameModal);
+
+function isGameOver() {
+    return pScore === 5 || cScore === 5; 
+}
+
+function handleClick(playerSelection) {
+    if(isGameOver()) {
+        openEndgameModal();
+        return;
+    }
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    update(playerSelection, computerSelection);
+    if(isGameOver()) {
+        openEndgameModal();
+        setFinalMessage();
+    }
+}
+
+function update(psel, csel) {
+    if(winner === 'tie') {
+        scoreInfo.textContent = 'It\'s a tie!';
+        scoreMessage.textContent = `${psel} ties with ${csel}`;
+    } else if(winner === 'player') {
+        scoreInfo.textContent = 'You won!';
+        scoreMessage.textContent = `${psel} beats ${csel}`;
+        pScore++;
+        playerScore.textContent = `Player: ${pScore}`;
+    } else if(winner === 'computer') {
+        scoreMessage.textContent = `${psel} is beaten by ${csel}`;
+        scoreInfo.textContent = 'You lost!';
+        cScore++;
+        computerScore.textContent = `Computer: ${cScore}`;
+    }
+    if(psel === 'rock') {
+        playerSign.textContent = '✊';
+    } else if (psel === 'paper') {
+        playerSign.textContent = '✋';
+    } else if (psel === 'scissors') {
+        playerSign.textContent = '✌';
+    }
+    if(csel === 'rock') {
+        computerSign.textContent = '✊';
+    } else if (csel === 'paper') {
+        computerSign.textContent = '✋';
+    } else if (csel === 'scissors') {
+        computerSign.textContent = '✌';
+    }
+}
+
+function openEndgameModal() {
+    endgameModal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeEndgameModal() {
+    endgameModal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+function setFinalMessage() {
+    return pScore > cScore 
+        ? (endgameMsg.textContent = 'You won')
+        : (endgameMsg.textContent = 'You lost')
+}
+
+function restartGame() {
+    pScore = 0;
+    cScore = 0;
+    scoreInfo.textContent = 'Choose move';
+    scoreMessage.textContent = 'First to 5 points wins';
+    playerScore.textContent = 'Player: 0';
+    computerScore.textContent = 'Computer: 0';
+    playerSign.textContent = '❔';
+    computerSign.textContent = '❔';
+    endgameModal.classList.remove('active');
+    overlay.classList.remove('active');
+  }
